83ff65ffd14679e8b5fd06efdbc8742d2353977c
Foster <72973015+Foster0123@users.noreply.github.com>
2023-01-17 00:53:34 +0530

    Chat Initial Commit 1

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// const msgBox = document.getElementById("chat-text-msg-input")
+// const chatBox = document.getElementById("chat-section-container")
+
+// msgBox.addEventListener('input', () => {
+// 	if(msgBox.clientHeight > 200){
+// 		msgBox.style.height = 200;
+// 	}
+// 	else {
+// 		msgBox.style.height = "30px";
+//     	msgBox.style.height = (msgBox.scrollHeight)+"px";
+// 	}
+	
+// })
