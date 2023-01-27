4d6a98c07cf7e9a1ee35eefd61149656962bd1bd
Foster <72973015+Foster0123@users.noreply.github.com>
2023-01-27 23:11:57 +0530

    Initial Commit 5 Chat

--- src/js/modules/inputField.js
+++ src/js/modules/inputField.js
@@ -2,17 +2,21 @@ var msgInputBox = document.getElementById("chat-text-msg-input")
 var chatBox = document.getElementById("chat-section-container")
 var msgInputPlaceholder = document.getElementById("msg-input-placeholder")
 
-if (msgInputBox.textContent.length > 0) {
-	msgInputPlaceholder.textContent = "";
-}
-else {
-	msgInputPlaceholder.textContent = "Type A Message..."
-}
-msgInputBox.addEventListener('input', () => {
+const InputField = () => {
 	if (msgInputBox.textContent.length > 0) {
 		msgInputPlaceholder.textContent = "";
 	}
 	else {
 		msgInputPlaceholder.textContent = "Type A Message..."
 	}
-})
+	msgInputBox.addEventListener('input', () => {
+		if (msgInputBox.textContent.length > 0) {
+			msgInputPlaceholder.textContent = "";
+		}
+		else {
+			msgInputPlaceholder.textContent = "Type A Message..."
+		}
+	})
+}
+
+export default InputField;
