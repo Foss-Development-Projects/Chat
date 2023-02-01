57f51f0b92f83aab36b757c7b0965a7a30a898e3
Foster <72973015+Foster0123@users.noreply.github.com>
2023-02-02 00:27:44 +0530

    Initial Commit 10

--- src/assets/js/modules/Emoji.js
+++ src/assets/js/modules/Emoji.js
@@ -1,22 +1,14 @@
 import { Picker } from 'emoji-mart'
 
-const getEmoji = (e) => {
-	return e.native;
-}
-const Emoji = async (dom, btn, inputArea, emojiPicker) => {
-	let a = {}
-	const pickerOptions = { onEmojiSelect: getEmoji(e), set: 'google', }
-	const picker = new Picker(pickerOptions)
+const Emoji = async (dom, btn, inputArea) => {
+	const picker = new Picker({ onEmojiSelect:console.log, set: 'google', })
 	await dom.appendChild(picker)
 	btn.addEventListener('click', () => {
 		dom.classList.toggle('show-emoji-menu')
-		console.log(a)
 	})
 	inputArea.addEventListener('focus', () => {
 		dom.className = "emoji-menu";
 	})
-	emojiPicker.addEventListener("select", () => {
-		console.log(getEmoji(e))
-	})
+	console.log(picker.native)
 }
 export default Emoji;
